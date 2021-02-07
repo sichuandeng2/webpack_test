2a841149f3d40ee7f6b15a6cca1f99ac0ac6f62d
sichuandeng2 <102633860@qq.com>
2021-02-07 12:15:14 +0800

    新增js兼容性处理功能

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+// import '@babel/polyfill'
+
 import './css/image.less';
 import './css/main.css';
 import './Iconfont/iconfont.css';
 
-function add(a, b) {
+const add = function add(a, b) {
   return a + b;
-}
+};
+
+const promise = new Promise((resolve) => {
+  setTimeout(() => {
+    // eslint-disable-next-line
+    console.log('定时器启动成功');
+    resolve();
+  }, 1000);
+});
+// eslint-disable-next-line
+console.log(promise)
 // eslint-disable-next-line
 console.log(add(6, 2));
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,32 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 
-// process.env.NODE_ENV = 'development'
-process.env.NODE_ENV = 'production'
+// 声明运行模式：development | production
+const runMode = 'development'
+
+// 定Css样式loader
+const commentCssLoader = [
+  {
+    loader: MiniCssExtractPlugin.loader,
+    options: {
+      publicPath: '../'
+    }
+  },
+  'css-loader',
+  {
+    loader: 'postcss-loader',
+    options:{
+      postcssOptions:{
+        plugins: () => {
+          'postcss-preset-env'
+        }
+      }
+    }
+  },
+]
+// 绑定node.js 的运行环境
+process.env.NODE_ENV = runMode
+// webpack的配置
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -16,48 +40,13 @@ module.exports = {
       {
         test: /\.less$/,
         use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: '../'
-            }
-          },
-          'css-loader',
+          ...commentCssLoader,
           'less-loader',
-          {
-            loader: 'postcss-loader',
-            options:{
-              postcssOptions:{
-                plugins: () => {
-                  'postcss-preset-env'
-                }
-              }
-            }
-          }
         ]
       },
       {
         test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: '../',
-              esModule: false
-            }
-          },
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options:{
-              postcssOptions:{
-                plugins: () => {
-                  'postcss-preset-env'
-                }
-              }
-            }
-          }
-        ],
+        use: [...commentCssLoader]
       },
       {
         test: /\.html$/,
@@ -69,7 +58,8 @@ module.exports = {
         options: {
           outputPath: 'imgs',
           limit: 12*1024,
-          name: '[hash:10].[ext]'
+          name: '[hash:10].[ext]',
+          esModule: false
         }
       },
       {
@@ -84,9 +74,37 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
+        enforce: 'pre',
         loader: 'eslint-loader',
         options: {
           fix: true
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            // '@babel/preset-env'
+            [
+              '@babel/preset-env',
+              {
+                useBuiltIns:'usage',
+                // useBuiltIns:'entry',
+                corejs: {
+                  version: 3
+                },
+                targets: {
+                  chrome: '60',
+                  firefox: '60',
+                  ie: '9',
+                  safari: '10',
+                  edge: '17'
+                }
+              }
+            ]
+          ]
         }
       }
     ]
@@ -94,16 +112,20 @@ module.exports = {
   plugins: [
     // html模板
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
+      // minify: {
+      //   collapseWhitespace: true,
+      //   removeComments: true
+      // }
     }),
     // css样式提取
     new MiniCssExtractPlugin({
       filename: 'css/index.css',
     }),
     // 压缩css文件
-    new OptimizeCssAssetsWebpackPlugin()
+    // new OptimizeCssAssetsWebpackPlugin()
   ],
-  mode: 'development',
+  mode: runMode,
   devServer: {
     contentBase: resolve(__dirname, 'dist'),
     compress: true,
